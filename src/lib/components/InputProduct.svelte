a95903a359f3809d62425dab50c51ec0038e242c
Chelo <marzelo.aguirre@gmail.com>
2022-02-01 10:23:54 -0300

    se agrego autocomplete

--- src/lib/components/InputProduct.svelte
+++ src/lib/components/InputProduct.svelte
@@ -1,22 +1,44 @@
 <script>
 	import { createEventDispatcher } from 'svelte';
-	import {products} from './productData.js'
+	import Autocomplete, { ListOption } from "./autocomplete/Autocomplete.svelte";
+	import {products} from './autocomplete/productData.js'
 	export let product = {
 		name: '',
 		quantity: 0
 	};
     const dispatch = createEventDispatcher();
+	let myValue = null;
+
+	let open = false;
+	const items = products;
+	//$: value = "";
+	//$: color = "";
+	$: colors = items.filter(c => c.includes(product.name));
+	const numbers = Array.from({ length: 50 }, (_, i) => i + 1);
 
     function returnProduct() {
 		dispatch('submit');
 	}
-	let myValue = null;
-
 </script>
 
 <div class="col-md-6 m-auto">
 	<div class="input-group mb-3">
-
+		
+		<Autocomplete 
+		id="cbo-0" 
+		name='example-1'
+		placeholder="Type a color…"
+		bind:value={product.name}
+	  >
+		{#if colors.length}
+		  {#each colors as color (color)}
+			<ListOption value={color} />
+		  {/each}
+		{:else} 
+		  <em>No Results…</em>
+		{/if}
+	  </Autocomplete>
+<!--
 		<input
 			type="text"
 			class="form-control"
@@ -24,7 +46,7 @@
 			aria-label="something to do"
 			aria-describedby="input_todo"
 			id="inputProduct"
-			bind:value={product.name} />
+			bind:value={product.name} />-->
 		<input
 			type="number"
 			class="form-control"
---
+++ src/lib/components/autocomplete/mutationObserver.js
@@ -0,0 +1,13 @@
+export default function mutationObserver(node, { callback, ...options }) {
+    let observer = new MutationObserver(callback);
+  
+    observer.observe(node, options);
+  
+    return {
+      destroy() {
+        observer.disconnect();
+        observer = null;
+      }
+    };
+  }
+  
